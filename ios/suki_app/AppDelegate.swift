15c1a6448412651925e99cec1c8becb509fedf81
Rshield001 <archielled21@gmail.com>
2025-07-17 22:28:55 +0800

    messaged comitted

--- ios/suki_app/AppDelegate.swift
+++ ios/suki_app/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "MyApp",
+      withModuleName: "suki_app",
       in: window,
       launchOptions: launchOptions
     )
